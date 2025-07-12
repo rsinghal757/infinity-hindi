e72fc01f8ab3545890d4f524650be10bb4dc191f
Rohit Singhal <your.email@example.com>
2025-07-12 16:02:47 +0530

    setup for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,13 @@
 import { defineConfig } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://rsinghal757.github.io",
+  base: "/infinity-hindi",
+  integrations: [partytown()],
   vite: {
     plugins: [tailwindcss()],
   },
